a92108a7fdc8f7c72e6fc7439a0b1cc0e6029237
virtimus <virtimus@gmail.com>
2020-02-23 12:35:48 +0100

    msn

--- dockerfiles/msn/i3c-build.sh
+++ dockerfiles/msn/i3c-build.sh
@@ -2,14 +2,15 @@
 #currently only ubuntu:bionic
 #cp $i3cScriptDir/dhcpcd.conf /etc/dhcpcd.conf
 if [ ! -e /etc/dnsmasq.conf.orig ]; then
-	apt-get update && apt-get -y install systemd dnsmasq hostapd dhcpcd5 && mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+	sudo apt-get update && sudo apt-get -y install systemd dnsmasq hostapd dhcpcd5 && mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 	
 	cp $i3cScriptDir/dhcpcd.conf /etc/dhcpcd.conf
 	cp $i3cScriptDir/dnsmasq.conf /etc/dnsmasq.conf
 	cp $i3cScriptDir/hostapd.conf /etc/hostapd/hostapd.conf 
 	cp $i3cScriptDir/wifistart /usr/local/bin/wifistart
+	cp $i3cScriptDir/../../wifistart /usr/local/bin/wifistart
 	chmod a+x /usr/local/bin/wifistart \
 	&& echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd \
-	&& echo '/bin/bash /usr/local/bin/wifistart' >> /etc/rc.local
+	&& echo '/i dup msn' >> /etc/rc.local
 fi
 
--- dockerfiles/msn/i3c-run.sh
+++ dockerfiles/msn/i3c-run.sh
@@ -7,3 +7,10 @@ dParams="-d --privileged --net host \
 		--cap-add SYS_ADMIN \
 		-v $uData/..:/msndata \
 		-v /etc/localtime:/etc/localtime:ro"
+		
+		
+i3cAfter(){
+	
+	/bin/bash /usr/local/bin/wifistart
+	
+}
